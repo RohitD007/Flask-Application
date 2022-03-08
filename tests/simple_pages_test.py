3766183f72d5042035cf538e8786b2dded1797ed
RohitD007 <dsouza.rohit.94@gmail.com>
2022-03-07 23:53:00 -0500

    Changes sample pages

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,20 +1,21 @@
 """This test the homepage"""
 
+
 def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'<a class="nav-link" href="/about">About</a>' in response.data
-    assert b'<a class="nav-link" href="/page1">GIT</a>' in response.data
-    assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
-    assert b'<a class="nav-link" href="/page3">Python and Flask</a>' in response.data
-    assert b'<a class="nav-link" href="/page4">CI/CD</a>' in response.data
+    assert b'About'in response.data
+    assert b'GIT' in response.data
+    assert b'Docker' in response.data
+    assert b'Python/Flask' in response.data
+    assert b'CI/CD' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Index Page" in response.data
+    assert b"Welcome to my Website!" in response.data
 
 def test_request_about(client):
     """This makes the index page"""
@@ -26,13 +27,13 @@ def test_request_page1(client):
     """This makes the index page"""
     response = client.get("/page1")
     assert response.status_code == 200
-    assert b"Page 1" in response.data
+    assert b"What is Git" in response.data
 
 def test_request_page2(client):
     """This makes the index page"""
     response = client.get("/page2")
     assert response.status_code == 200
-    assert b"Page 2" in response.data
+    assert b"What is Docker?" in response.data
 
 def test_request_page3(client):
     """This makes the index page"""
